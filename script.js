52c05ef4706dca5dae22aad9dfc0af59738af726
eihabobeidat <obeidateihab9@gmail.com>
2021-11-21 21:10:04 +0200

    commit test

--- script.js
+++ script.js
@@ -106,10 +106,10 @@ function lose()
 	let figth = document.createElement('img');
 	figth.src = "rasengan.gif";
 	figth.style.paddingLeft = "26%";
-	figth.hight="50%";
-	let anime = document.createElement('img');
+// 	figth.hight="50%";
+// 	let anime = document.createElement('img');
 	anime.src = "sad.gif";
-	let sound = document.createElement('audio');
+// 	let sound = document.createElement('audio');
 	sound.src = "los.mp4";
 	sound.type= "video/mp4";
 	sound.autoplay= true;
